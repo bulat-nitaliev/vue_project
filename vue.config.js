7a2d645221673ca619687f5a309f24691be2405a
bulat nitaliev <bulatnitaliev2016@gmail.com>
2024-05-21 12:49:18 +0400

    vue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/learn_project/',
+  publicPath: '/vue_project/',
   transpileDependencies: true
 })
